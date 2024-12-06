ad0cd63764e3c1394f9948e126d5d6e0909f95ee
Simona Pizio <simona.pizio@gmail.com>
2024-12-06 16:06:06 +0100

    Add nested routes

--- src/app/App.js
+++ src/app/App.js
@@ -18,13 +18,17 @@ import "./App.css";
 
 const router = createBrowserRouter( createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
-    <Route path="/about" element={<About/>}></Route>
-    <Route path="/sign-up" element={<SignUp/>}></Route>
-    <Route path="/articles" element={<Articles/>}></Route>
-    <Route path="/articles/:title" element={<Article/>}></Route>
-    <Route path="/authors/:name" element={<Author/>}></Route>
-    <Route path="/categories" element={<Categories/>}></Route>
-    <Route path="/profile" element={<Profile/>}></Route>
+    <Route path="about" element={<About/>}/>
+    <Route path="sign-up" element={<SignUp/>}/>
+    <Route path="articles" element={<Articles/>}/>
+    <Route path="articles/:title" element={<Article/>}/>
+    <Route path="authors/:name" element={<Author/>}/>
+    <Route path="categories" element={<Categories/>}>
+      <Route path=":name" element={<Category/>}/>
+    </Route>
+    <Route path="profile" element={<Profile/>}>
+      <Route path="edit" element={<EditProfileForm/>}/>
+    </Route>
   </Route>
 ));
 
